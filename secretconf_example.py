ed97589a57c9c785ff44845b4787c890ff0f4922
Sam Frances <sam@samfrances.uk>
2015-08-07 12:19:15 +0100

    secretconf example and update .gitignore

---
+++ secretconf_example.py
@@ -0,0 +1,6 @@
+"""Things I don't want in my git repo"""
+
+CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
+ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
